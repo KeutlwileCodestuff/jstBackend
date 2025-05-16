fff29e9737b984804d459e44dd709acd22a0c9a3
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-05-16 17:36:13 +0200

    log in endpoint complete

--- two/src/routes/todoRoutes.js
+++ two/src/routes/todoRoutes.js
@@ -4,7 +4,25 @@ import DB from '../db.js'
 const router = express.Router()
 
 // get all todos for logged user
+router.get('/' , (req , res) => {
+    const getTodos = DB.prepare('SELECT * FROM todo WHERE user_id= ? ')
+    const todos = getTodos.all(req.user_id )
+    res.json({todos})
 
+
+})
+
+router.post('/' , (req , res) => {
+    
+})
+
+router.put('/' , (req , res) => {
+    
+})
+
+router.delete('/' , (req , res) => {
+    
+})
 // Create a new todo
 
 // update a todo
