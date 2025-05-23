576c3c1497777f3c7f8229921ce729b33aa30ee8
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-05-23 22:45:01 +0200

    set Docker instractions

--- three/src/routes/authRoutes.js
+++ three/src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import DB from '../db.js'
 import e from 'express'
 import prisma from '../prismaClient.js'
                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
